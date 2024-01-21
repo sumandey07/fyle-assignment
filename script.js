f53755b5e892fcaaf79932a95eb8ba108827145f
Suman <sumankumardey89@gmail.com>
2024-01-21 21:09:30 +0530

    update

--- script.js
+++ script.js
@@ -1,6 +1,84 @@
-let searchRepos, filter, li, reposList, i, a, txtValue;
+let username = document.getElementById("username");
+let searchRepos, newJson, filter, reposList, i, txtValue, texts, list, newLink;
 let loader = document.getElementById("loader");
 let form = document.getElementById("form");
+let errorHandler = document.getElementById("errorHandler");
+let perPageCounters = document.getElementById("perPageCounters");
+let currentPage = 1,
+  perPage = 10;
+
+function repoLoad(usernames) {
+  showLoader();
+  fetch(
+    `https://api.github.com/users/${usernames.value}/repos?page=1&per_page=${perPage}`,
+    {
+      method: "GET",
+      headers: {
+        Authorization:
+          "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", //access token for using github api with specific scopes
+      },
+    }
+  ).then((resp) => {
+    newLink = parseLink(resp.headers.get("link"));
+    resp.json().then((val) => {
+      Object.keys(val).forEach((key) => {
+        let details2 = "",
+          details3 = `<div id="topics" class="d-flex overflow-x-hidden flex-row mx-1 gap-2 mt-4"></div>`;
+
+        const details1 = `<div id="name" class="fs-3 fw-bold">${val[key].name}</div>`;
+
+        // paginate(val, perPage);
+
+        if (val[key].description !== null) {
+          details2 = `<div id="desc" class="mt-4">
+                  ${val[key].description}
+                </div>`;
+        } else {
+          details2 = `<div id="desc" class="mt-4 fs-5"></div>`;
+        }
+        newJson = JSON.stringify(val[key].topics);
+        newJson = JSON.parse(newJson);
+
+        // if (newJson.length === 0) {
+        //   details3 = `<div id="topic" class="align-items-center fw-semibold text-white p-1 rounded-1"></div>`;
+        //   document
+        //     .getElementById("topics")
+        //     .insertAdjacentText("beforeend", details3);
+        // } else {
+        //   details3 = `<div id="topic" class="align-items-center fw-semibold text-white p-1 rounded-1">${newJson}</div>`;
+        //   document
+        //     .getElementById("topics")
+        //     .insertAdjacentHTML("beforeend", details3);
+        // }
+
+        const details = `<div id="repoContainer" class="bg-light bg-gradient col-5 rounded-1 border border-2 p-3 my-4 border-dark d-flex flex-column">${details1} ${details2} ${details3}</div>`;
+
+        document
+          .getElementById("reposList")
+          .insertAdjacentHTML("beforeend", details);
+      });
+    });
+  });
+  document.getElementById("reposList").innerHTML = "";
+}
+
+function parseLink(s) {
+  const output = {};
+  const regex = /<([^>]+)>; rel="([^"]+)"/g;
+
+  let m;
+  while ((m = regex.exec(s))) {
+    const [_, v, k] = m;
+    output[k] = v;
+  }
+
+  return output;
+}
+
+window.onload = function () {
+  username.focus();
+  searchFunc();
+};
 
 function showLoader() {
   loader.classList.add("show");
@@ -10,38 +88,72 @@ function hideLoader() {
   loader.classList.remove("show");
 }
 
-function searchFunc() {
-  searchRepos = document.getElementById("searchRepos");
-  filter = searchRepos.value.toLowerCase();
-  reposList = document.getElementById("reposList");
-  li = ul.getElementsByTagName("li");
-  for (i = 0; i < li.length; i++) {
-    a = li[i].getElementsByTagName("a")[0];
-    txtValue = a.textContent || a.innerText;
-    if (txtValue.toLowerCase().indexOf(filter) > -1) {
-      li[i].style.display = "";
-    } else {
-      li[i].style.display = "none";
-    }
-  }
+function showError() {
+  hideLoader();
+  errorHandler.classList.add("show");
+}
+
+function hideError() {
+  errorHandler.classList.remove("show");
+}
+
+function perPageCounter() {
+  perPage = perPageCounters.value;
+  repoLoad(username);
+}
+
+function paginate(items, itemsPerPage, paginationContainer) {
+  // const totalPages = Math.ceil(items.length / itemsPerPage);
+  // function showItems(page) {
+  //   const startIndex = (page - 1) * itemsPerPage;
+  //   const endIndex = startIndex + itemsPerPage;
+  //   const pageItems = items.slice(startIndex, endIndex);
+  //   const itemsContainer = document.querySelector("#items");
+  //   itemsContainer.innerHTML = "";
+  //   pageItems.forEach((item) => {
+  //     const li = document.createElement("li");
+  //     li.innerText = item;
+  //     itemsContainer.appendChild(li);
+  //   });
+  // }
+  // function setupPagination() {
+  //   const pagination = document.querySelector(paginationContainer);
+  //   pagination.innerHTML = "";
+  //   for (let i = 1; i <= totalPages; i++) {
+  //     const link = document.createElement("a");
+  //     link.href = "#";
+  //     link.innerText = i;
+  //     if (i === currentPage) {
+  //       link.classList.add("active");
+  //     }
+  //     link.addEventListener("click", (event) => {
+  //       event.preventDefault();
+  //       currentPage = i;
+  //       showItems(currentPage);
+  //       const currentActive = pagination.querySelector(".active");
+  //       currentActive.classList.remove("active");
+  //       link.classList.add("active");
+  //     });
+  //     pagination.appendChild(link);
+  //   }
+  // }
+  // showItems(currentPage);
+  // setupPagination();
 }
 
 form.addEventListener("submit", function (e) {
   e.preventDefault();
-  let username = document.getElementById("username");
   const userUrl = `https://api.github.com/users/${username.value}`;
-  // URLSearchParams.append();
-  let params = new URLSearchParams(document.location.search);
-  username = params.get("username");
   showLoader();
-  let original = document.getElementById("userDetails").innerHTML;
   fetch(userUrl, {
     method: "GET",
     headers: {
-      Authorization: "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", //access token for using github api with specific scopes
+      Authorization:
+        "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", //access token for using github api with specific scopes
     },
   }).then((res) => {
     if (res.status !== 404) {
+      hideError();
       res.json().then((data) => {
         hideLoader();
         document.getElementById("main").style.height = "max-content";
@@ -51,60 +163,36 @@ form.addEventListener("submit", function (e) {
         document.getElementById("bio").innerHTML = data.bio;
         document.getElementById("location").innerHTML = data.location;
         document.getElementById("profileLink").innerHTML = data.html_url;
-        document.getElementById("twitterLink").innerHTML =
-          data.twitter_username;
+        document.getElementById("profileLink").href = data.html_url;
+        document.getElementById(
+          "twitterLink"
+        ).innerHTML = `https://twitter.com/${data.twitter_username}`;
+        document.getElementById(
+          "twitterLink"
+        ).href = `https://twitter.com/${data.twitter_username}`;
         document.getElementById("profileImage").src = data.avatar_url;
-        fetch(`${userUrl}/repos?page=1&per_page=10`, {
-          method: "GET",
-          headers: {
-            Authorization: "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", //access token for using github api with specific scopes
-          },
-        }).then(async (resp) => {
-          for (var pair of resp.headers.entries()) {
-            if (pair[0] === "link") {
-              // this.setState({
-              //   total: pair[1],
-              // });
-            }
-          }
-          return resp.json().then((val) => {
-            Object.keys(val).forEach((key) => {
-              let details2 = "",
-                details3 = "";
-
-              const details1 = `<div id="name" class="fs-4 fw-bold">${val[key].name}</div>`;
-
-              if (val[key].description !== null) {
-                details2 = `<div id="desc" class="mt-3">
-                  ${val[key].description}
-                </div>`;
-              } else {
-                details2 = `<div id="desc" class="mt-3"></div>`;
-              }
-
-              Object.keys(val[key].topics).forEach((key1) => {
-                details3 = `<div id="topics" class="d-flex flex-row me-1 mt-2"><span class="badge text-bg-primary">${val[key].topics[key1]}</span></div>`;
-
-                // document
-                //   .getElementById("topics")
-                //   .insertAdjacentHTML("beforeend", details3);
-              });
-
-              const details = `<div class="col-5 border border-2 p-3 my-3 border-dark d-flex flex-column">${details1} ${details2} ${details3}</div>`;
-
-              document
-                .getElementById("reposList")
-                .insertAdjacentHTML("beforeend", details);
-            });
-          });
-        });
-        document.getElementById("reposList").innerHTML = "";
+        repoLoad(username);
       });
     } else {
-      const errorHandler = `<h3 class="position-absolute top-50 start-50 translate-middle">Your search did not match any users</h3>`;
-      document.getElementById("userDetails").style.display = "block";
-      document.getElementById("userDetails").innerHTML = errorHandler;
+      document.getElementById("title").innerHTML = "User Not Found - Github";
+      document.getElementById("mainContainer").style.display = "none";
+      showError();
     }
   });
-  document.getElementById("userDetails").innerHTML = original;
 });
+
+function searchFunc() {
+  searchRepos = document.getElementById("searchRepos").value.toLowerCase();
+  reposList = document.getElementById("reposList");
+  list = document.querySelectorAll("#repoContainer");
+  filter = reposList.getElementsByTagName("div");
+  for (i = 0; i < filter.length; i++) {
+    txtValue = list[i].getElementsByTagName("div");
+    texts = txtValue.innerHTML || txtValue.textContent;
+    if (texts.toLowerCase().indexOf(searchRepos) > -1) {
+      list[i].style.display = "";
+    } else {
+      list[i].style.display = "none";
+    }
+  }
+}
